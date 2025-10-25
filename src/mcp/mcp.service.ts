8a5ee93d1d44359a9ac5ce7b159c0f1c9e424a5f
Ahmed Salah Fathy Sotohy <laslsalsa123@gmail.com>
2025-10-25 22:03:57 +0300

    feat(mcp): refactor McpService methods and remove unused functions

--- src/mcp/mcp.service.ts
+++ src/mcp/mcp.service.ts
@@ -81,20 +81,9 @@ export class McpService {
       };
     }
   }
-
+  
 
   
     
 
-  findOne(id: number) {
-    return `This action returns a #${id} mcp`;
-  }
-
-  update(id: number) {
-    return `This action updates a #${id} mcp`;
-  }
-
-  remove(id: number) {
-    return `This action removes #${id} mcp`;
-  }
 }
