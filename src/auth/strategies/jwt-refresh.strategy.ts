b77a139a75ae520938f8888f4f9e158f9fd66bcf
Ahmed Salah Fathy Sotohy <laslsalsa123@gmail.com>
2025-12-25 20:57:56 +0200

    feat(auth): Implement JwtRefreshStrategy for handling JWT refresh tokens

---
+++ src/auth/strategies/jwt-refresh.strategy.ts
@@ -0,0 +1,27 @@
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { AuthService } from "../auth.service";
+
+@Injectable()
+export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
+    constructor(
+        private config: ConfigService,
+        private authService: AuthService,
+    ) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: config.get('JWT_REFRESH_SECRET')!,
+            ignoreExpiration: false,
+        });
+    }
+
+
+     validate(payload: RefreshJwtPayload) {
+        return this.authService.validateRefreshToken(
+            payload.sub,
+            payload.tokenId,
+        ); 
+    };
+};
